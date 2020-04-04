e6434297006fda8183039995077e172e96c9859d
siwei <siweilxy@163.com>
2020-04-04 19:22:33 +0800

    remvoe log

--- common/log.cpp
+++ common/log.cpp
@@ -79,20 +79,19 @@ int log::init(int stderrFlag)
     {
         level = atoi(plevel);
     }
-
     FLAGS_stderrthreshold = level;
     FLAGS_logbufsecs = 0; //立即写入
 
     FLAGS_logtostderr = false;  //是否打印到控制台
     FLAGS_alsologtostderr = false;  //打印到日志同时是否打印到控制台
 
-    if(stderrFlag == 1)
-    {
-        FLAGS_stderrthreshold = 3;
-    }else
-    {
-        FLAGS_stderrthreshold = 0;
-    }
+//    if(stderrFlag == 1)
+//    {
+//        FLAGS_stderrthreshold = 3;
+//    }else
+//    {
+//        FLAGS_stderrthreshold = 0;
+//    }
 
     FLAGS_colorlogtostderr = true; //标准输出带颜色
 
