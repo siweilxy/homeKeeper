c10b5aa24d1bff7ff5def298836c9c4fb10c86ad
siweilxy <siweilxy@163.com>
2020-10-28 10:33:37 +0800

    add hmlog

---
+++ include/hmLog.hpp
@@ -0,0 +1,21 @@
+/*
+ * hmLog.hpp
+ *
+ *  Created on: 2020年10月28日
+ *      Author: siwei
+ */
+
+#ifndef INCLUDE_HMLOG_HPP_
+#define INCLUDE_HMLOG_HPP_
+
+#include <stdio.h>
+class hmLog
+{
+	hmLog()
+	{
+		printf("hmLog 启动");
+	}
+};
+
+
+#endif /* INCLUDE_HMLOG_HPP_ */
---
+++ test/test.cpp
@@ -0,0 +1,17 @@
+/*
+ * test.cpp
+ *
+ *  Created on: 2020年10月28日
+ *      Author: siwei
+ */
+
+#include <hmLog.hpp>
+
+
+int main()
+{
+	hmLog hmlog;
+	return 0;
+}
+
+
