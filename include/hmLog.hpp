5bc6f42996da5e93ca4ff86ec739cf3acdbd2309
siweilxy <siweilxy@163.com>
2020-10-28 11:25:43 +0800

    fix bug

--- include/hmLog.hpp
+++ include/hmLog.hpp
@@ -9,6 +9,10 @@
 #define INCLUDE_HMLOG_HPP_
 
 #include <stdio.h>
+#include <vector>
+#include <thread>
+#include <memory>
+
 class hmLog
 {
 public:
@@ -20,14 +24,45 @@ public:
 
 	~hmLog()
 	{
+		printThread->join();
 		printf("hmLog 结束\n");
+		insertLog("hmLog 结束\n");
 	}
 
 private:
 	hmLog()
 	{
 		printf("hmLog 启动\n");
+		printThread =  new std::thread(printLog);
+		insertLog("hmLog 启动\n");
 	}
+
+	void printLog()
+	{
+		logsMutex.lock();
+
+		auto logsTemp = std::move(logs);
+
+		for(auto log :logsTemp)
+		{
+			printf("%s",log.c_str());
+		}
+
+		logsMutex.unlock();
+	}
+
+	void insertLog(std::string log)
+	{
+		logsMutex.lock();
+
+		logs.push_back(log);
+
+		logsMutex.unlock();
+	}
+
+	std::vector<std::string> logs;
+	std::mutex logsMutex;
+	std::unique_ptr<std::thread> printThread;
 };
 
 
