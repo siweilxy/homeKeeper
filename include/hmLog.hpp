f865f958fa0e5a7f6429edeec461e62ee6bb6d3f
siweilxy <siweilxy@163.com>
2020-11-01 20:30:02 +0800

    fix bug

--- include/hmLog.hpp
+++ include/hmLog.hpp
@@ -195,7 +195,7 @@ void* printLog(void *para) {
 				char temBuf[11240]={0};
 				snprintf(temBuf,sizeof(temBuf),"%s:%s:%s\n", log_str[log.level],log.time, log.msg);
 				printf("%s",temBuf);
-				hmLog::getInstance().logfile->write(temBuf);
+				hmLog::getInstance().logfile->write(temBuf,sizeof(temBuf));
 			}
 		}
 
