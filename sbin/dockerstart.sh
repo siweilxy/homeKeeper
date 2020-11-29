1794cfc68d09712e81db0ad3eec1a0f221a7a452
siweilxy <siweilxy@163.com>
2020-11-29 17:08:07 +0800

    add exec env

--- sbin/dockerstart.sh
+++ sbin/dockerstart.sh
@@ -1,4 +1,4 @@
-docker run -d -p 3306:3306 -v /home/siwei/mysql/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD="root" mysql
+docker run -d -p 3306:3306 -v /root/work/mysql/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD="root" mysql
 docker run -p 6379:6379 --name redis  -v /home/siwei/redis/data:/data  -d redis --appendonly yes --requirepass "789"
 docker run -p 2181:2181 -p 9092:9092 -dit spotify/kafka:latest
 
