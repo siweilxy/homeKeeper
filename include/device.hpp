7349c2a928c0a6051aee1f409aea050fc18ef50c
siweilxy <siweilxy@163.com>
2020-12-14 10:43:52 +0800

    add node

---
+++ include/device.hpp
@@ -0,0 +1,86 @@
+/*
+ * device.hpp
+ *
+ *  Created on: 2020年12月12日
+ *      Author: siwei
+ */
+
+#ifndef INCLUDE_DEVICE_HPP_
+#define INCLUDE_DEVICE_HPP_
+
+#include "hmLog.hpp"
+#include <map>
+#include <functional>
+
+enum deviceType
+{
+	TCP_SERVER = 0,
+	TCP_CLIENT,
+	UDP_SERVER,
+	UDP_SERVER_BROADCAST,
+	UDP_CLIENT
+};
+
+
+
+class device
+{
+private:
+
+	int tcpServer(void* para)
+	{
+		INFO("tcp server");
+		return 0;
+	}
+
+	int tcpClient(void* para)
+	{
+		INFO("tcp client");
+		return 0;
+	}
+
+	int udpServer(void* para)
+	{
+		INFO("udp server");
+		return 0;
+	}
+
+	int udpServerBroadcast(void* para)
+	{
+		INFO("udp Server broadcast");
+		return 0;
+	}
+
+	int udpClient(void* para)
+	{
+		INFO("udp client");
+		return 0;
+	}
+
+	std::map<int ,std::function<int(void*)>> funcs;
+	int type;
+public:
+	device (int type,int socket):type(type)
+	{
+		funcs[TCP_SERVER] = tcpServer;
+		funcs[TCP_CLIENT] = tcpClient;
+		funcs[UDP_SERVER] = udpServer;
+		funcs[UDP_SERVER_BROADCAST] = udpServerBroadcast;
+		funcs[UDP_CLIENT] = udpClient;
+	}
+
+	int init()
+	{
+		if(type >= funcs.size())
+		{
+			ERROR("TYPE ERROR");
+			return -1;
+		}
+
+		return funcs[type]();;
+	}
+};
+
+
+
+#endif /* INCLUDE_DEVICE_HPP_ */
--- node/node.cpp
+++ node/node.cpp
@@ -7,11 +7,26 @@
 #include <unistd.h>
 #include <stdio.h>
 #include "hmLog.hpp"
+#include "device.hpp"
 
 int main()
 {
-	hmLog::getInstance().init(0);
+	int ret = 0;
+	ret = hmLog::getInstance().init(0);
+    if(ret != 0)
+    {
+        printf("log init error");
+        return -1;
+    }
+
 	INFO("node 启动");
+	device d(UDP_SERVER_BROADCAST,0);
+	ret = d.init();
+	if(ret != 0)
+	{
+		ERROR("d init failed");
+	}
+
 	return 0;
 }
 
